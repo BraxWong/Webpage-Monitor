c32fc518ebfc93d331d135668f9785ae53b32bb2
Brax Wong <ghostenjob@gmail.com>
2024-04-14 20:30:53 +0100

    feature: able to obtain item name and price.

--- src/main.py
+++ src/main.py
@@ -1,8 +1,9 @@
 import emailSender
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 import time
-emailAddress = input("Please input your email address: ")
-password = input("Please input your password: ")
+# emailAddress = input("Please input your email address: ")
+# password = input("Please input your password: ")
 
 options = webdriver.ChromeOptions()
 options.add_argument('start-maximized')
@@ -10,14 +11,24 @@ options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) Apple
 options.add_experimental_option("prefs", {"profile.managed_default_content_settings.images": 2})
 options.add_experimental_option("excludeSwitches", ["enable-automation"])
 options.add_experimental_option('useAutomationExtension', False)
+options.headless = True
 url = 'https://skinport.com/tf2/market?quality=12&sort=date&order=desc'
 driver = webdriver.Chrome(options=options)
 try:
     driver.get(url)
+    #Wait 30 seconds for the browser to load before getting info
+    time.sleep(30)
+    items = driver.find_elements(By.CLASS_NAME, 'ItemPreview-commonInfo')
+    for item in items:
+        #TODO: Should write to file to check if all items are the same 
+        itemName = item.find_element(By.CLASS_NAME, 'ItemPreview-itemName').text
+        itemPrice = item.find_element(By.CLASS_NAME, 'Tooltip-link').text
+        print(itemName, itemPrice)
     while True:
-        pass
+        continue
 except:
-    emailSender.sendEmail(emailAddress, password, "Warning!! Monitor Has Been Terminated", "An error has taken place and the program has terminated. Please reboot as soon as possible") 
+    print("Something went wrong")
+    # emailSender.sendEmail(emailAddress, password, "Warning!! Monitor Has Been Terminated", "An error has taken place and the program has terminated. Please reboot as soon as possible") 
 
 
 
